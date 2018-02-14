befc3862d568f256a41aa579f537367cdfa7bdf9
Prathmesh Pathak <prathmeshpathak@Prathmeshs-MacBook-Air.local>
2018-02-14 22:44:40 +0530

    revoke the changes

--- problems-to-solve/Recursion/factorial_iterative.py
+++
@@ -1,9 +0,0 @@
-def iterative_factorial(n):
-    result = 1
-    for i in range(2,n+1):
-        result *= i
-    return result
-
-
-for i in range(1, 100):
-    print(i , ":", iterative_factorial(i))
