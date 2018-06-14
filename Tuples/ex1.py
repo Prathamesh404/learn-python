bf990040eba46130c98bee1e4ebb6beab61b78be
medi <pprathamesh1594@gmail.com>
2018-06-14 20:50:17 +0530

    Understanding the built-ins

---
+++ Tuples/ex1.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+
+# Understanding size difference.
+import sys
+
+
+
+
+
---
+++ Tuples/example.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+#Tuple.
+perfect_square = (1,4,9,16, 25, 36, 49, 64)
+
+for i in perfect_square:
+    print(f"The squares are {i}")
+
